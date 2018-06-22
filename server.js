bb5d884332f4f852ec3f3da8c8cd4e69320b1b9d
Tyler McNamara <mcnamara14@gmail.com>
2018-06-22 12:57:40 -0600

    Add basic server functionality

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
+app.use(urlLogger, timeLogger);
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
   response.send('hello world');
+});
+
+app.get('/json', (request, response) => {
+  response.status(200).json({"name": "Robbie"});
 });
 
 app.listen(3000, () => {
